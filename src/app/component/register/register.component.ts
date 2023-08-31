250fb9c2004d6f94cf37075cbc5e8bb061042ba2
msrecec <msrecec@3plus.hr>
2023-08-31 02:06:33 +0200

    added registration form

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {UserService} from "../../service/user/user.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent {
+    username: string = ""
+    email: string = ""
+    password: string = ""
+    firstName: string = ""
+    lastName: string = ""
+    dob: string = ""
+    description: string | null = null
+
+
+    constructor(private userService: UserService) {
+    }
+
+    register() {
+        this.userService.register(this.username, this.password, this.email, this.firstName, this.lastName, new Date(this.dob), this.description).subscribe()
+    }
 
 }
--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -6,34 +6,34 @@ import {SERVER_API_URL} from "../../constants/app.constants";
 import {ErrorHandlerService} from "../error/error-handler.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService implements OnInit {
-  currentUser: User | null | undefined
-  private usersUrl = `${SERVER_API_URL}/users`;
-  private registerURL: string = `${this.usersUrl}/register`;
+    currentUser: User | null | undefined
+    private usersUrl = `${SERVER_API_URL}/users`;
+    private registerURL: string = `${this.usersUrl}/register`;
 
-  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
-  }
+    constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
+    }
 
-  ngOnInit(): void {
-    this.currentUser = null;
-  }
+    ngOnInit(): void {
+        this.currentUser = null;
+    }
 
-  public getCurrentUser(): Observable<User> {
-    return this.http.get<User>(`${this.usersUrl}/current-user`);
-  }
+    public getCurrentUser(): Observable<User> {
+        return this.http.get<User>(`${this.usersUrl}/current-user`);
+    }
 
-  public register(username: string, password: string, email: string, firstName: string, lastName: string, dateOfBirth: Date, description: string) {
-    return this.http.post<User>(this.registerURL, {
-      username: username,
-      password: password,
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      dateOfBirth: dateOfBirth,
-      description: description
-    }).pipe(catchError(this.errorHandlerService.handleError))
-  }
+    public register(username: string, password: string, email: string, firstName: string, lastName: string, dateOfBirth: Date, description: string | null) {
+        return this.http.post<User>(this.registerURL, {
+            username: username,
+            password: password,
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            dateOfBirth: dateOfBirth,
+            description: description
+        }).pipe(catchError(this.errorHandlerService.handleError))
+    }
 
 }
