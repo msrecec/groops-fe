84826fc3e12804c7ff2e90b17a6113dea9fbd3e8
msrecec <msrecec@3plus.hr>
2023-09-07 02:26:37 +0200

    added upload

--- src/app/component/profile-edit/profile-edit.component.ts
+++ src/app/component/profile-edit/profile-edit.component.ts
@@ -1,123 +1,141 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UserService} from "../../service/user/user.service";
 import {Router} from "@angular/router";
 import {catchError, throwError} from "rxjs";
-import {CONFIRM_EMAIL} from "../../constants/app.constants";
+import {CONFIRM_EMAIL, PROFILE} from "../../constants/app.constants";
 import {Error} from "../../model/error.model";
 import {User} from "../../model/user.model";
 import {transitionAnimation} from "../../animation/transition.animation";
 
 @Component({
-  selector: 'app-profile-edit',
-  templateUrl: './profile-edit.component.html',
-  styleUrls: ['./profile-edit.component.css'],
-  animations: [transitionAnimation]
+    selector: 'app-profile-edit',
+    templateUrl: './profile-edit.component.html',
+    styleUrls: ['./profile-edit.component.css'],
+    animations: [transitionAnimation]
 })
 export class ProfileEditComponent implements OnInit {
-  profilePicture: string = ""
-  username: string = ""
-  password: string = ""
-  firstName: string = ""
-  lastName: string = ""
-  dob: string = ""
-  description: string | null = null
-  errorToggle: Boolean = false
-  usernameRequiredError: string = ""
-  usernameTakenError: string = ""
-  emailRequiredError: string = ""
-  emailFormatError: string = ""
-  passwordRequiredError: string = ""
-  passwordValidationMessages: String[] = []
-  firstNameRequiredError: string = ""
-  lastNameRequiredError: string = ""
-  dobRequiredError: string = ""
+    profilePicture: string = ""
+    username: string = ""
+    password: string = ""
+    firstName: string = ""
+    lastName: string = ""
+    dob: string = ""
+    description: string | null = null
+    errorToggle: Boolean = false
+    usernameRequiredError: string = ""
+    usernameTakenError: string = ""
+    emailRequiredError: string = ""
+    emailFormatError: string = ""
+    passwordRequiredError: string = ""
+    passwordValidationMessages: String[] = []
+    firstNameRequiredError: string = ""
+    lastNameRequiredError: string = ""
+    dobRequiredError: string = ""
+    fileToUpload: File | null = null
+    localUrl: any[] | null = null;
 
+    constructor(private userService: UserService, private router: Router) {
 
-  constructor(private userService: UserService, private router: Router) {
+    }
 
-  }
+    ngOnInit(): void {
+        this.userService.getCurrentUser().subscribe((user) => this.editProfileComponents(user))
+    }
 
-  ngOnInit(): void {
-    this.userService.getCurrentUser().pipe(catchError(err => this.showErrorMessage(err))).subscribe((user) => {
-      this.username = user.username.toString()
-      this.firstName = user.firstName.toString()
-      this.lastName = user.lastName.toString()
-      this.dob = user.dateOfBirth.toString()
-      this.description = user.description.toString()
-      this.profilePicture = user.profilePictureDownloadLink.toString()
-    })
-  }
+    updateProfile() {
+        this.usernameRequiredError = ""
+        this.usernameTakenError = ""
+        this.emailRequiredError = ""
+        this.emailFormatError = ""
+        this.passwordRequiredError = ""
+        this.passwordValidationMessages = []
+        this.firstNameRequiredError = ""
+        this.lastNameRequiredError = ""
+        this.dobRequiredError = ""
+        this.userService.updateUser(this.username, this.firstName, this.lastName, new Date(this.dob), this.description !== null ? this.description : '', this.fileToUpload)
+            .pipe(catchError(err => this.showErrorMessage(err)))
+            .subscribe(() => this.toProfile());
+    }
 
-  updateProfile() {
-    this.usernameRequiredError = ""
-    this.usernameTakenError = ""
-    this.emailRequiredError = ""
-    this.emailFormatError = ""
-    this.passwordRequiredError = ""
-    this.passwordValidationMessages = []
-    this.firstNameRequiredError = ""
-    this.lastNameRequiredError = ""
-    this.dobRequiredError = ""
-    this.userService.updateUser(this.username, this.firstName, this.lastName, new Date(this.dob), this.description !== null ? this.description : '')
-      .subscribe((user) => {
+    private editProfileComponents(user: User) {
         this.username = user.username.toString()
         this.firstName = user.firstName.toString()
         this.lastName = user.lastName.toString()
         this.dob = user.dateOfBirth.toString()
         this.description = user.description.toString()
         this.profilePicture = user.profilePictureDownloadLink.toString()
-      });
-  }
-
-  private showErrorMessage(errorRes: Error) {
-    let errorMessage = 'An unknown error occurred';
-
-    if (errorRes != null) {
-      this.errorToggle = true
-      if (errorRes.message != null) {
-        if (errorRes.message.includes("duplicate key value violates unique constraint \"user_username_un_idx\"")) {
-          this.usernameTakenError = "username taken"
-          return throwError(() => new Error(false, errorRes.message, errorRes.status))
-        }
-        let errorMessages = errorRes.message.split(';')
-        for (let message of errorMessages) {
-          const messageTrimmed = message.trim()
-          if (message.includes("Password")) {
-            this.passwordValidationMessages.push(message)
-            continue
-          }
-          switch (messageTrimmed) {
-            case 'username is required': {
-              this.usernameRequiredError = 'username is required'
-              break
-            }
-            case 'first name is required': {
-              this.firstNameRequiredError = 'first name is required'
-              break
-            }
-            case 'last name is required': {
-              this.lastNameRequiredError = 'last name is required'
-              break
-            }
-            case 'date of birth is required': {
-              this.dobRequiredError = 'date of birth is required'
-              break
-            }
-            case 'format must be a valid email': {
-              this.emailFormatError = 'format must be a valid email'
-              break
-            }
-            case 'password is required': {
-              this.passwordRequiredError = 'password is required'
-              break
-            }
-          }
-        }
-        return throwError(() => new Error(false, errorRes.message, errorRes.status))
-      }
     }
-    return throwError(() => errorMessage);
-  }
 
-  protected readonly String = String;
+    private toProfile() {
+        this.router.navigate([`/${PROFILE}`]).then(() => this.handleNavigation(PROFILE));
+    }
+
+    private handleNavigation(route: string) {
+        console.log(`Navigating to ${route} page`)
+    }
+
+    showPreviewImageAndSetFile(event: any) {
+        if (event.target.files && event.target.files[0]) {
+            this.localUrl = null;
+            let reader = new FileReader();
+            reader.onload = (event: any) => {
+                this.localUrl = event.target.result;
+            }
+            this.fileToUpload = event.target.files[0]
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    }
+
+
+    private showErrorMessage(errorRes: Error) {
+        let errorMessage = 'An unknown error occurred';
+
+        if (errorRes != null) {
+            this.errorToggle = true
+            if (errorRes.message != null) {
+                if (errorRes.message.includes("duplicate key value violates unique constraint \"user_username_un_idx\"")) {
+                    this.usernameTakenError = "username taken"
+                    return throwError(() => new Error(false, errorRes.message, errorRes.status))
+                }
+                let errorMessages = errorRes.message.split(';')
+                for (let message of errorMessages) {
+                    const messageTrimmed = message.trim()
+                    if (message.includes("Password")) {
+                        this.passwordValidationMessages.push(message)
+                        continue
+                    }
+                    switch (messageTrimmed) {
+                        case 'username is required': {
+                            this.usernameRequiredError = 'username is required'
+                            break
+                        }
+                        case 'first name is required': {
+                            this.firstNameRequiredError = 'first name is required'
+                            break
+                        }
+                        case 'last name is required': {
+                            this.lastNameRequiredError = 'last name is required'
+                            break
+                        }
+                        case 'date of birth is required': {
+                            this.dobRequiredError = 'date of birth is required'
+                            break
+                        }
+                        case 'format must be a valid email': {
+                            this.emailFormatError = 'format must be a valid email'
+                            break
+                        }
+                        case 'password is required': {
+                            this.passwordRequiredError = 'password is required'
+                            break
+                        }
+                    }
+                }
+                return throwError(() => new Error(false, errorRes.message, errorRes.status))
+            }
+        }
+        return throwError(() => errorMessage);
+    }
+
+    protected readonly String = String;
 }
--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -1,49 +1,76 @@
 import {Injectable, OnInit} from '@angular/core';
 import {User} from "../../model/user.model";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from "@angular/common/http";
 import {catchError, Observable} from "rxjs";
-import {SERVER_API_URL} from "../../constants/app.constants";
+import {GROOPS_TOKEN, SERVER_API_URL} from "../../constants/app.constants";
 import {ErrorHandlerService} from "../error/error-handler.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService implements OnInit {
-  currentUser: User | null | undefined
-  private usersUrl = `${SERVER_API_URL}/users`;
-  private registerURL: string = `${this.usersUrl}/register`;
+    currentUser: User | null | undefined
+    private usersUrl = `${SERVER_API_URL}/users`;
+    private registerURL: string = `${this.usersUrl}/register`;
+    localUrl: any[] = [];
 
-  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
-  }
+    constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) {
+    }
 
-  ngOnInit(): void {
-    this.currentUser = null;
-  }
+    ngOnInit(): void {
+        this.currentUser = null;
+    }
 
-  public getCurrentUser(): Observable<User> {
-    return this.http.get<User>(`${this.usersUrl}/current`);
-  }
+    public getCurrentUser(): Observable<User> {
+        return this.http.get<User>(`${this.usersUrl}/current`);
+    }
 
-  public updateUser(username: string, firstName: string, lastName: string, dateOfBirth: Date, description: string): Observable<User> {
-    return this.http.put<User>(`${this.usersUrl}/current`, {
-      username,
-      firstName,
-      lastName,
-      dateOfBirth,
-      description
-    }).pipe(catchError(this.errorHandlerService.handleError));
-  }
+    public updateUser(username: string, firstName: string, lastName: string, dateOfBirth: Date, description: string, file: any | null): Observable<any> {
+        const command = {
+            username,
+            firstName,
+            lastName,
+            dateOfBirth,
+            description
+        };
+        if (file !== null) {
+            const token = localStorage.getItem(GROOPS_TOKEN);
+            if (token === null) {
+                throw new Error("Token missing");
+            }
+            const formData: FormData = new FormData();
+            formData.append('file1', file);
 
-  public register(username: string, password: string, email: string, firstName: string, lastName: string, dateOfBirth: Date, description: string | null) {
-    return this.http.post<User>(this.registerURL, {
-      username: username,
-      password: password,
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      dateOfBirth: dateOfBirth,
-      description: description
-    }).pipe(catchError(this.errorHandlerService.handleError))
-  }
+            const blob = new Blob([JSON.stringify(command)], {
+                type: 'application/json',
+            });
+            formData.append('file2', blob);
+            const xhr = new XMLHttpRequest()
+            xhr.onreadystatechange = (e) => {
+                if (xhr.status === 200) {
+                    console.log('SUCCESS', xhr.responseText);
+                } else {
+                    console.warn('request_error');
+                }
+            };
+            xhr.open('POST', `${this.usersUrl}/current/upload-profile`, false);
+            xhr.setRequestHeader('Authorization', token.toString())
+            xhr.send(formData);
+            return new Observable()
+        }
+        return this.http.put(`${this.usersUrl}/current`, command);
+    }
+
+    public register(username: string, password: string, email: string, firstName: string, lastName: string, dateOfBirth: Date, description: string | null) {
+        return this.http.post<User>(this.registerURL, {
+            username: username,
+            password: password,
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            dateOfBirth: dateOfBirth,
+            description: description
+        }).pipe(catchError(this.errorHandlerService.handleError))
+    }
 
 }
