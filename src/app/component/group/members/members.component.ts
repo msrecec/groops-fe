6d818633e130e2aa1a7c2abf45a2180a668376b6
msrecec <msrecec@3plus.hr>
2023-09-17 20:14:36 +0200

    qf

--- src/app/component/group/members/members.component.ts
+++ src/app/component/group/members/members.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import {User} from "../../../model/user.model";
+import {Component} from '@angular/core';
 import {GroupService} from "../../../service/group/group.service";
 import {ErrorHandlerService} from "../../../service/error/error-handler.service";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -10,80 +9,86 @@ import {UserRole} from "../../../model/user.role";
 import {UserService} from "../../../service/user/user.service";
 
 @Component({
-  selector: 'app-members',
-  templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css'],
-  animations: [transitionAnimation]
+    selector: 'app-members',
+    templateUrl: './members.component.html',
+    styleUrls: ['./members.component.css'],
+    animations: [transitionAnimation]
 })
 export class MembersComponent {
-  currentUserId: Number = -1
-  users: UserRole[] = []
-  currentlyChaningRoles: Set<Number> = new Set<Number>()
-  selectedOption: string = "ROLE_USER"
+    currentUserId: Number = -1
+    role: RoleEnum = RoleEnum.ROLE_LURKER
+    users: UserRole[] = []
+    currentlyChaningRoles: Set<Number> = new Set<Number>()
+    selectedOption: string = "ROLE_USER"
 
-  constructor(private groupService: GroupService, private errorHandlerService: ErrorHandlerService, private router: Router, private route: ActivatedRoute,
-              private userService: UserService) {
-  }
-
-  ngOnInit(): void {
-    const groupId = this.route.snapshot.paramMap.get("id");
-    if (!groupId) {
-      console.error("Missing group id in param map")
-      return
+    constructor(private groupService: GroupService, private errorHandlerService: ErrorHandlerService, private router: Router, private route: ActivatedRoute,
+                private userService: UserService) {
     }
-    this.userService.getCurrentUser().pipe(
-      catchError(this.errorHandlerService.handleError)
-    ).subscribe((user) => {
-      this.currentUserId = user.id
-      this.groupService.getMembersByGroupId(groupId).pipe(
-        catchError(this.errorHandlerService.handleError)
-      ).subscribe(
-        (users) => {
-          this.users = users
+
+    ngOnInit(): void {
+        const groupId = this.route.snapshot.paramMap.get("id");
+        if (!groupId) {
+            console.error("Missing group id in param map")
+            return
         }
-      )
-    })
-  }
-
-  changeRole(userId: Number, role: RoleEnum) {
-    const groupId = this.route.snapshot.paramMap.get("id");
-    if (!groupId) {
-      console.error("Missing group id in param map")
-      return
+        this.userService.getCurrentUser().pipe(
+            catchError(this.errorHandlerService.handleError)
+        ).subscribe((user) => {
+            this.currentUserId = user.id
+            this.groupService.getCurrentUserRoleEnum(groupId).pipe(
+                catchError(this.errorHandlerService.handleError)
+            ).subscribe(role => {
+                this.role = role
+                this.groupService.getMembersByGroupId(groupId).pipe(
+                    catchError(this.errorHandlerService.handleError)
+                ).subscribe(
+                    (users) => {
+                        this.users = users
+                    }
+                )
+            })
+        })
     }
-    this.groupService.changeRole(userId.toString(), groupId, role)
-      .pipe(catchError(this.errorHandlerService.handleError))
-      .subscribe(() => {
-        this.currentlyChaningRoles = new Set<Number>()
-        this.groupService.getMembersByGroupId(groupId).pipe(
-          catchError(this.errorHandlerService.handleError)
-        ).subscribe(
-          (users) => {
-            this.users = users
-          }
-        )
-      })
-  }
 
-  kickUser(userId: Number) {
-    const groupId = this.route.snapshot.paramMap.get("id");
-    if (!groupId) {
-      console.error("Missing group id in param map")
-      return
+    changeRole(userId: Number, role: RoleEnum) {
+        const groupId = this.route.snapshot.paramMap.get("id");
+        if (!groupId) {
+            console.error("Missing group id in param map")
+            return
+        }
+        this.groupService.changeRole(userId.toString(), groupId, role)
+            .pipe(catchError(this.errorHandlerService.handleError))
+            .subscribe(() => {
+                this.currentlyChaningRoles = new Set<Number>()
+                this.groupService.getMembersByGroupId(groupId).pipe(
+                    catchError(this.errorHandlerService.handleError)
+                ).subscribe(
+                    (users) => {
+                        this.users = users
+                    }
+                )
+            })
     }
-    this.groupService.kickUserFromGroup(userId.toString(), groupId)
-      .pipe(catchError(this.errorHandlerService.handleError))
-      .subscribe(() => {
-        this.currentlyChaningRoles = new Set<Number>()
-        this.groupService.getMembersByGroupId(groupId).pipe(
-          catchError(this.errorHandlerService.handleError)
-        ).subscribe(
-          (users) => {
-            this.users = users
-          }
-        )
-      })
-  }
 
-  protected readonly RoleEnum = RoleEnum;
+    kickUser(userId: Number) {
+        const groupId = this.route.snapshot.paramMap.get("id");
+        if (!groupId) {
+            console.error("Missing group id in param map")
+            return
+        }
+        this.groupService.kickUserFromGroup(userId.toString(), groupId)
+            .pipe(catchError(this.errorHandlerService.handleError))
+            .subscribe(() => {
+                this.currentlyChaningRoles = new Set<Number>()
+                this.groupService.getMembersByGroupId(groupId).pipe(
+                    catchError(this.errorHandlerService.handleError)
+                ).subscribe(
+                    (users) => {
+                        this.users = users
+                    }
+                )
+            })
+    }
+
+    protected readonly RoleEnum = RoleEnum;
 }
