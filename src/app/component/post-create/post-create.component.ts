5bac384ffc173956e4c33b4d399e2edf99c3ac4d
msrecec <msrecec@3plus.hr>
2023-09-17 04:35:27 +0200

    qf

--- src/app/component/post-create/post-create.component.ts
+++ src/app/component/post-create/post-create.component.ts
@@ -20,23 +20,9 @@ export class PostCreateComponent {
   imgLoaded = false;
   profilePicture: string = ""
   profilePictureThumbnail: string = ""
-  username: string = ""
-  password: string = ""
-  firstName: string = ""
-  lastName: string = ""
-  dob: string = ""
   description: string | null = null
   errorToggle: Boolean = false
   errorMessage: string = ''
-  usernameRequiredError: string = ""
-  usernameTakenError: string = ""
-  emailRequiredError: string = ""
-  emailFormatError: string = ""
-  passwordRequiredError: string = ""
-  passwordValidationMessages: String[] = []
-  firstNameRequiredError: string = ""
-  lastNameRequiredError: string = ""
-  dobRequiredError: string = ""
   fileToUpload: File | null = null
   localUrl: any[] | null = null;
   isSpinning = false
@@ -46,7 +32,7 @@ export class PostCreateComponent {
   }
 
   ngOnInit(): void {
-    this.userService.getCurrentUser().subscribe((user) => this.editProfileComponents(user))
+
   }
 
   onLoad(image: HTMLImageElement) {
@@ -54,49 +40,6 @@ export class PostCreateComponent {
     this.imgLoaded = true
   }
 
-  updateProfile() {
-    this.usernameRequiredError = ""
-    this.usernameTakenError = ""
-    this.emailRequiredError = ""
-    this.emailFormatError = ""
-    this.passwordRequiredError = ""
-    this.passwordValidationMessages = []
-    this.firstNameRequiredError = ""
-    this.lastNameRequiredError = ""
-    this.dobRequiredError = ""
-    this.isSpinning = true
-    this.errorToggle = false
-    if (this.fileToUpload) {
-      const fileCommand: UserUpdateFileCommand = new UserUpdateFileCommand(this.username.trim(), this.firstName.trim(), this.lastName.trim(), new Date(this.dob.trim()), this.description ? this.description.trim() : "", this.fileToUpload)
-      this.userService.updateUserWithFile(fileCommand).pipe(catchError((err) => {
-        this.isSpinning = false;
-        return this.showErrorMessage(err)
-      })).subscribe(() => {
-        this.isSpinning = false
-        this.toProfile()
-      })
-      return
-    }
-    const command: UserCommand = new UserCommand(this.username.trim(), this.firstName.trim(), this.lastName.trim(), new Date(this.dob.trim()), this.description ? this.description.trim() : "")
-    this.isSpinning = true
-    this.userService.updateUserWithoutFile(command).pipe(catchError((err) => {
-      this.isSpinning = false;
-      return this.showErrorMessage(err)
-    })).subscribe(() => {
-      this.isSpinning = false
-      this.toProfile()
-    })
-  }
-
-  private editProfileComponents(user: User) {
-    this.username = user.username.toString()
-    this.firstName = user.firstName.toString()
-    this.lastName = user.lastName.toString()
-    this.dob = user.dateOfBirth.toString()
-    this.description = user.description.toString()
-    this.profilePicture = user.profilePictureDownloadLink ? user.profilePictureDownloadLink.toString() : ''
-    this.profilePictureThumbnail = user.profilePictureThumbnailDownloadLink ? user.profilePictureThumbnailDownloadLink.toString() : ''
-  }
 
   private toProfile() {
     this.router.navigate([`/${PROFILE}`]).then(() => this.handleNavigation(PROFILE));
@@ -118,6 +61,10 @@ export class PostCreateComponent {
     }
   }
 
+  post() {
+
+  }
+
 
   private showErrorMessage(error: HttpErrorResponse) {
     let errorMessage = 'An unknown error occurred';
@@ -130,46 +77,11 @@ export class PostCreateComponent {
       this.errorToggle = true
       if (errorRes.message != null) {
         if (errorRes.message.includes("duplicate key value violates unique constraint \"user_username_un_idx\"")) {
-          this.usernameTakenError = "username taken"
           return throwError(() => new Error(false, errorRes.message, errorRes.status))
         }
         let errorMessages = errorRes.message.split(';')
         for (let message of errorMessages) {
           const messageTrimmed = message.trim()
-          if (message.includes("Password")) {
-            this.passwordValidationMessages.push(message)
-            continue
-          }
-          switch (messageTrimmed) {
-            case 'username is required': {
-              this.usernameRequiredError = 'username is required'
-              break
-            }
-            case 'first name is required': {
-              this.firstNameRequiredError = 'first name is required'
-              break
-            }
-            case 'last name is required': {
-              this.lastNameRequiredError = 'last name is required'
-              break
-            }
-            case 'date of birth is required': {
-              this.dobRequiredError = 'date of birth is required'
-              break
-            }
-            case 'format must be a valid email': {
-              this.emailFormatError = 'format must be a valid email'
-              break
-            }
-            case 'password is required': {
-              this.passwordRequiredError = 'password is required'
-              break
-            }
-            case 'Text must not be blank': {
-              this.firstNameRequiredError = 'Text must not be blank'
-              break
-            }
-          }
           this.errorMessage = messageTrimmed
         }
         return throwError(() => new Error(false, errorRes.message, errorRes.status))
